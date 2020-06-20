8e97b02b8733e954d0282ca02638885387a61d38
ABY001 <45406011+ABY001@users.noreply.github.com>
2020-06-20 21:04:14 +0100

    First commit

---
+++ pages/about/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about-page">
+    <h1>This</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sunt fugit
+      vero, ipsam veritatis recusandae, atque animi sit excepturi nulla odit
+      tenetur libero explicabo dolorum possimus ducimus temporibus, repellat
+      corporis?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sunt fugit
+      vero, ipsam veritatis recusandae, atque animi sit excepturi nulla odit
+      tenetur libero explicabo dolorum possimus ducimus temporibus, repellat
+      corporis?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sunt fugit
+      vero, ipsam veritatis recusandae, atque animi sit excepturi nulla odit
+      tenetur libero explicabo dolorum possimus ducimus temporibus, repellat
+      corporis?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui sunt fugit
+      vero, ipsam veritatis recusandae, atque animi sit excepturi nulla odit
+      tenetur libero explicabo dolorum possimus ducimus temporibus, repellat
+      corporis?
+    </p>
+  </div>
+</template>
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
